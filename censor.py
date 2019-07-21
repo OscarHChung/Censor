3dc9343c179f738971920ac9c5baac85317ea31b
Oscar Chung <47995398+OscarHChung@users.noreply.github.com>
2019-07-21 12:41:46 -0700

    Create censor.py

---
+++ censor.py
@@ -0,0 +1,44 @@
+from cs50 import get_string
+from sys import argv
+
+words = set()
+
+def main():
+
+    if len(argv) != 2:
+        print("Usage: python bleep.py dictionary")
+        exit(1)
+
+    load(argv[1])
+    input = get_string("What messasge would you like to censor? ")
+    tokens = input.split()
+    limit = 0
+
+    for i in tokens:
+        if check(i):
+            for j in i:
+                print("*", end="")
+        else:
+            print(i, end="")
+
+        limit = limit + 1
+
+        if limit != len(tokens):
+            print(" ", end="")
+
+    print("\n")
+    return True
+
+def load(banned):
+    file = open(banned, "r")
+    for line in file:
+        words.add(line.rstrip("\n"))
+    file.close()
+    return True
+
+def check(word):
+    return word.lower() in words
+
+
+if __name__ == "__main__":
+    main()
